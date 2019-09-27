328ced7bc3611eb63b1079d17078b46f8824610a
Gabriel Rigon <gabrielrigon@outlook.com>
2019-09-27 13:19:11 -0300

    Adds and configure bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
+const bodyParser = require('body-parser')
 const debug = require('debug')('d3-growth:app')
+const express = require('express')
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => debug(`listening on port ${port}`))
