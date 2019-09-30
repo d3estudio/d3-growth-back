dd45869ca5e1b10f156604ff9397ac337354d0be
Gabriel Rigon <gabrielrigon@outlook.com>
2019-09-30 16:26:01 -0300

    Adds answer service and implements unit tests

---
+++ tests/services/answer.test.js
@@ -0,0 +1,259 @@
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should()
+
+const mocha = require('mocha'),
+  describe = mocha.describe
+
+const sinon = require('sinon'),
+  sandbox = sinon.createSandbox()
+
+const answerService = require('../../app/services/answer')
+const keywordsData = require('../../app/utils/keywords.json')
+
+describe('AnswerService', () => {
+  describe('normalizeAnswerString(answer)', () => {
+    describe('when the param is not present', () => {
+      it('should return an empty string', () => {
+        answerService
+          .normalizeAnswerString('')
+          .should.be.a('string')
+          .with.lengthOf(0)
+      })
+    })
+
+    describe('when the param is present', () => {
+      describe('and contains accent or cedilha', () => {
+        it('should return normalized string', () => {
+          assert.equal(answerService.normalizeAnswerString('çãúâAñö çñ'), 'cauaano cn')
+        })
+      })
+
+      describe('and is a number', () => {
+        it('should convert number into a string', () => {
+          assert.equal(answerService.normalizeAnswerString(12345), '12345')
+        })
+      })
+    })
+  })
+
+  describe('keywordsEntries(step)', () => {
+    describe('when the param is not present', () => {
+      it('should return an empty array', () => {
+        answerService
+          .keywordsEntries('')
+          .should.be.a('array')
+          .with.lengthOf(0)
+      })
+    })
+
+    describe('when the param is present', () => {
+      describe('and the step not exists in json', () => {
+        it('should return an empty array', () => {
+          answerService
+            .keywordsEntries('xyz')
+            .should.be.a('array')
+            .with.lengthOf(0)
+        })
+      })
+
+      describe('and the value exists in json', () => {
+        const { categorias, epicos, temas } = keywordsData
+
+        it('should return correct array length for categories', () => {
+          answerService
+            .keywordsEntries('categorias')
+            .should.be.a('array')
+            .with.lengthOf(Object.entries(categorias).length)
+        })
+
+        it('should return correct array length for epics', () => {
+          answerService
+            .keywordsEntries('epicos')
+            .should.be.a('array')
+            .with.lengthOf(Object.entries(epicos).length)
+        })
+
+        it('should return correct array length for themes', () => {
+          answerService
+            .keywordsEntries('temas')
+            .should.be.a('array')
+            .with.lengthOf(Object.entries(temas).length)
+        })
+      })
+    })
+  })
+
+  describe('testAnswer(answer, key)', () => {
+    describe('when the params are not present', () => {
+      it('should return zero', () => {
+        assert.equal(answerService.testAnswer('', null), 0)
+      })
+    })
+
+    describe('when the params are present', () => {
+      describe('and value don`t matches', () => {
+        it('should return zero', () => {
+          assert.equal(answerService.testAnswer('value matches', 'xyz'), 0)
+        })
+      })
+
+      describe('and value matches', () => {
+        it('should return 1', () => {
+          assert.equal(answerService.testAnswer('value matches', 'value'), 1)
+        })
+      })
+    })
+  })
+
+  describe('assurance(answer, points)', () => {
+    describe('when the params are not present', () => {
+      it('should return zero', () => {
+        assert.equal(answerService.assurance('', 0), 0)
+      })
+    })
+
+    describe('when the params are present', () => {
+      describe('and the text is fully matched', () => {
+        it('should return 1.0', () => {
+          assert.equal(answerService.assurance('lorem ipsum dolor', 3), 1.0)
+        })
+      })
+
+      describe('and the text is partial matched', () => {
+        it('should return 0.5', () => {
+          assert.equal(answerService.assurance('lorem ipsum', 1), 0.5)
+        })
+
+        it('should return 0.33', () => {
+          assert.equal(answerService.assurance('lorem ipsum dolor', 1), 0.33)
+        })
+      })
+    })
+  })
+
+  describe('checkStepKeywords(answer, step)', () => {
+    describe('when the params are not present', () => {
+      it('should return an empty object', () => {
+        expect(answerService.checkStepKeywords('', '')).to.be.empty
+      })
+    })
+
+    describe('when the params are present', () => {
+      it('should have correct entries for categories', () => {
+        const keys = Object.keys(keywordsData.categorias)
+        expect(answerService.checkStepKeywords('', 'categorias')).to.have.all.keys(keys)
+      })
+
+      it('should have correct entries for epics', () => {
+        const keys = Object.keys(keywordsData.epicos)
+        expect(answerService.checkStepKeywords('', 'epicos')).to.have.all.keys(keys)
+      })
+
+      it('should have correct entries for themes', () => {
+        const keys = Object.keys(keywordsData.temas)
+        expect(answerService.checkStepKeywords('', 'temas')).to.have.all.keys(keys)
+      })
+    })
+  })
+
+  describe('mostCompatible(summary)', () => {
+    describe('when the param is not present', () => {
+      it('should return null', () => {
+        assert.equal(answerService.mostCompatible(null), null)
+      })
+    })
+
+    describe('when the param is present', () => {
+      it('should return a', () => {
+        const summary = { a: 1.0, b: 0.8, c: 0.9 }
+        assert.equal(answerService.mostCompatible(summary), 'a')
+      })
+
+      it('should return c', () => {
+        const summary = { a: 0.1, b: 0.8, c: 1.0 }
+        assert.equal(answerService.mostCompatible(summary), 'c')
+      })
+    })
+  })
+
+  describe('classifyStep(answer, step)', () => {
+    describe('when the params are not present', () => {
+      it('should return null', () => {
+        assert.equal(answerService.classifyStep('', ''), null)
+      })
+    })
+
+    describe('when the params are present', () => {
+      describe('and the step is categories', () => {
+        it('should return funcionalidade', () => {
+          const answer = keywordsData.categorias.funcionalidade.toString()
+          assert.equal(answerService.classifyStep(answer, 'categorias'), 'funcionalidade')
+        })
+
+        it('should return conteudo', () => {
+          const answer = keywordsData.categorias.conteudo.toString()
+          assert.equal(answerService.classifyStep(answer, 'categorias'), 'conteudo')
+        })
+      })
+
+      describe('and the step is epics', () => {
+        it('should return boletos', () => {
+          const answer = keywordsData.epicos.boletos.toString()
+          assert.equal(answerService.classifyStep(answer, 'epicos'), 'boletos')
+        })
+
+        it('should return extrato', () => {
+          const answer = keywordsData.epicos.extrato.toString()
+          assert.equal(answerService.classifyStep(answer, 'epicos'), 'extrato')
+        })
+      })
+
+      describe('and the step is themes', () => {
+        it('should return a-mrv', () => {
+          const answer = keywordsData.temas['a-mrv'].toString()
+          assert.equal(answerService.classifyStep(answer, 'temas'), 'a-mrv')
+        })
+
+        it('should return usabilidade', () => {
+          const answer = keywordsData.temas.usabilidade.toString()
+          assert.equal(answerService.classifyStep(answer, 'temas'), 'usabilidade')
+        })
+      })
+    })
+  })
+
+  describe(' classify(answer)', () => {
+    describe('when the param is not present', () => {
+      it('should return an empty object', () => {
+        expect(answerService.classify('')).to.be.empty
+      })
+    })
+
+    describe('when the param is present', () => {
+      it('should have correct entries for epics', () => {
+        const keys = ['answer', 'theme', 'epic', 'category']
+        expect(answerService.classify('xyz')).to.have.all.keys(keys)
+      })
+    })
+  })
+
+  describe('classifyAll', () => {
+    describe('when the param is not present', () => {
+      it('should return an empty array', () => {
+        answerService
+          .classifyAll(null)
+          .should.be.a('array')
+          .with.lengthOf(0)
+      })
+    })
+
+    describe('when the param is present', () => {
+      it('should have classified answers', () => {
+        const keys = ['abc', 'xyz']
+        assert.lengthOf(answerService.classifyAll(keys), keys.length)
+      })
+    })
+  })
+})
