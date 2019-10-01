ccae33a56f9fe859ced1faf072b39d06939ec1ec
Gabriel Rigon <gabrielrigon@outlook.com>
2019-10-01 14:54:09 -0300

    Adds simplistic first version of auth process

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const passport = require('passport')
 const router = require('./app/router')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.API_PORT
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+
 app.use(passport.initialize())
 
 app.use(router)
