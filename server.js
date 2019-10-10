c6a14902cfab8610604281061f3b073dc23e9f60
Gabriel Rigon <gabrielrigon@outlook.com>
2019-10-10 16:21:29 -0300

    Removes update database on start server

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const passport = require('passport')
 
 const router = require('./app/router')
 
-const trackSaleService = require('./app/services/thirdParty/trackSale')
-
 const app = express()
 const API_PORT = process.env.API_PORT
 
@@ -16,10 +14,5 @@ app.use(passport.initialize())
 app.use(router)
 
 app.listen(API_PORT, () => console.log(`listening on port ${API_PORT}`))
-
-trackSaleService
-  .updateDatabase()
-  .then(result => console.log('database updated'))
-  .catch(err => console.error(err))
 
 module.exports = app
